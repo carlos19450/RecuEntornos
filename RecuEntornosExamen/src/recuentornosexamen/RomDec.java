066b2679fc3280a16d1d01a292f16e2fd7c71915
carlo <carlo@DESKTOP-PEA8BI3>
2022-05-26 16:08:42 +0200

    Creación del Javadoc para los metodos de la clase RomDec.

--- RecuEntornosExamen/src/recuentornosexamen/RomDec.java
+++ RecuEntornosExamen/src/recuentornosexamen/RomDec.java
@@ -9,11 +9,24 @@ package recuentornosexamen;
  * @author carlo
  */
 public class RomDec {
+
+    /**
+     * Método para convertir los nujmeros romanos a decimales.
+     *
+     * @param rom Variable de tipo String.
+     * @return Devuelve la conversión de un numero romano a decimal.
+     */
     public int roman2dec(String rom) {
         // Este metodo esta por implementar
         return 21;
     }
 
+    /**
+     * Método para convertir los nujmeros decimales a romanos.
+     *
+     * @param dec Variable de tipo int.
+     * @return Devuelve la conversión de un numero decimal a romano.
+     */
     public String dec2roman(int dec) {
         // Este metodo esta por implementar
         return "XXI";
