c28b5eaddca73cd7f31bfa093853661909c9dc7b
carlo <carlo@DESKTOP-PEA8BI3>
2022-05-26 15:52:23 +0200

    Métodos creados para la clase TestTemperaturas.

--- RecuEntornosExamen/src/recuentornosexamen/TestTemperaturas.java
+++ RecuEntornosExamen/src/recuentornosexamen/TestTemperaturas.java
@@ -9,5 +9,21 @@ package recuentornosexamen;
  * @author carlo
  */
 public class TestTemperaturas {
-    
+    public static double fahrenheittocelsius(double num) {
+        return (num - 32) / 1.8;
+    }
+
+    public static double celsiustofahrenheit(double num) {
+        return (num * 1.8) + 32;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double celsius = 23, fahrenheit = 5;
+        System.out.println(fahrenheittocelsius(fahrenheit));
+        System.out.println(celsiustofahrenheit(celsius));
+    }
 }
