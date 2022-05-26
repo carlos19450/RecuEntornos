a194d7a406aeef8b1314cca35e3882c86b79ca34
carlo <carlo@DESKTOP-PEA8BI3>
2022-05-26 16:05:21 +0200

    Creación del Javadoc para los metodos de la clase DolarEuro.

--- RecuEntornosExamen/src/recuentornosexamen/DolarEuro.java
+++ RecuEntornosExamen/src/recuentornosexamen/DolarEuro.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package recuentornosexamen;
 
 /**
@@ -9,10 +5,21 @@ package recuentornosexamen;
  * @author carlo
  */
 public class DolarEuro {
-        public double dollar2euro(double dollar) {
+
+    /**
+     * Método para convertir los dolares a euros.
+     * @param dollar Variable de tipo double.
+     * @return Devuelve la variable dollar convertidos a euros.
+     */
+    public double dollar2euro(double dollar) {
         return (dollar * 0.88);
     }
 
+    /**
+     * Método para convertir los euros a dolares.
+     * @param euro Variable de tipo double.
+     * @return Devuelve la variable dollar convertidos a dolares.
+     */
     public double euro2dollar(double euro) {
         return (euro * 1.13);
     }
