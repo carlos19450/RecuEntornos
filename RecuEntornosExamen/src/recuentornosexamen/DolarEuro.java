472ec3ba76ab20fa7ccd74adf4943714f0ccf3f3
carlo <carlo@DESKTOP-PEA8BI3>
2022-05-26 15:51:23 +0200

    Métodos creados para la clase DolarEuro.

--- RecuEntornosExamen/src/recuentornosexamen/DolarEuro.java
+++ RecuEntornosExamen/src/recuentornosexamen/DolarEuro.java
@@ -9,5 +9,11 @@ package recuentornosexamen;
  * @author carlo
  */
 public class DolarEuro {
-    
+        public double dollar2euro(double dollar) {
+        return (dollar * 0.88);
+    }
+
+    public double euro2dollar(double euro) {
+        return (euro * 1.13);
+    }
 }
